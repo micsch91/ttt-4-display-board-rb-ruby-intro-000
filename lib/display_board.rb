4cb4e80f7ae0e4fa690a0d7e78a0c08a051a4ff7
Michael Schneider <mic.sch91@gmail.com>
2020-02-27 03:10:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -55,13 +55,13 @@ def display_board(board)
 
 end
 
-board = ["O", "O", "O", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
 def display_board(board)
 
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
