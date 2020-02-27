c2296e5cc8f6a86a7ef025df829ae3c555392213
Michael Schneider <mic.sch91@gmail.com>
2020-02-27 02:13:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 # out the current state.
   def display_board(board)
 
-puts "#{   |   |   }"
-puts "#{-----------}"
-puts "#{   |   |   }"
-puts "#{-----------}"
-puts "#{   |   |   }"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
 
   end
