f6e1f997330ab5ecffad566b29e7af6b867009ea
Michael Schneider <mic.sch91@gmail.com>
2020-02-27 03:08:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -58,7 +58,7 @@ end
 board = ["O", "O", "O", " ", " ", " ", " ", " ", " "]
 def display_board(board)
 
-  puts " O | O | O "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
