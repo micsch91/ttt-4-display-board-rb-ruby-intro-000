7c139703aac71c30f8cd9c10a7ff2fcd7fa4c4cd
Michael Schneider <mic.sch91@gmail.com>
2020-02-27 02:38:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
 
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+puts "   |   |   "
+puts "-----------"
+puts "   | #{board[4]} |   "
 puts "-----------"
 puts "   |   |   "
 
