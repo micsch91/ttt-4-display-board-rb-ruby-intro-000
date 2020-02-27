5df57687e3b2d212cd3ca6133b7ac51c4e3d22e6
Michael Schneider <mic.sch91@gmail.com>
2020-02-27 03:21:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -65,3 +65,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+def display_board(board)
+
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   | #{board[8]} "
